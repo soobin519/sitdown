157343019cade990caa955252f1f2fb9ea5afd14
shshshee <sehhe22@naver.com>
2021-10-10 17:18:40 +0900

    설정파일 수정

--- demo/src/main/java/com/example/demo/RetryDemo1Application.java
+++ demo/src/main/java/com/example/demo/RetryDemo1Application.java
@@ -7,17 +7,27 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 @MapperScan(value= {"com/example/demo/DAO"})
-public class RetryDemo1Application {
+public class RetryDemo1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetryDemo1Application.class, args);
 	}
+	
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) 
+	{ 
+		return builder.sources(RetryDemo1Application.class); 
+	}
+
+	
 	@Bean
 	public SqlSessionFactory sqlSEssionFacoty(DataSource dataSource) throws Exception{
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
